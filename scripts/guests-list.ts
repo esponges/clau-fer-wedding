3e1f7909d8afcdb0af6196101ad0b0b537d01bc7
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-07-29 10:31:30 -0600

    add list to fix build

---
+++ scripts/guests-list.ts
@@ -0,0 +1,11 @@
+export const list: {
+  name: string,
+  status: 'pending' | 'rejected' | 'confirmed',
+  pax: number,
+}[] = [
+  {
+    name: 'Fer y Vicky',
+    status: 'pending',
+    pax: 2
+  }
+];
