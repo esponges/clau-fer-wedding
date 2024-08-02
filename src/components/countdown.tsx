f5fc0236f7a97e808e0cccb8040bf999b26ed08a
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-08-02 16:26:45 -0600

    corrected countdown

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,16 +1,72 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const initialCount = {
+  days: 0,
+  hours: 0,
+  mins: 0,
+  secs: 0
+};
+
 export function CountDown({ date }: { date: Date }) {
-  const toMexicoLocale = date.toLocaleString('es-MX', {
-    timeZone: 'America/Mexico_City',
-  });
+  const [count, setCount] = useState<{
+    days: number;
+    hours: number;
+    mins: number;
+    secs: number;
+  }>(initialCount);
+
+  useEffect(() => {
+    function getDateTillEvent() {
+      const now = new Date().getTime();
+      const diffInMs = date.getTime() - now;
+      const diffInSecs = Math.floor(diffInMs / 1000);
+
+      if (diffInSecs < 0) {
+        return initialCount;
+      }
+
+      const secsToDays = 86400; // 24 * 60 * 60
+      const secsToHours = 3600; // 60 * 60
+      const secsToMins = 60;
+
+      const days = Math.floor(diffInSecs / secsToDays);
+      const hours = Math.floor((diffInSecs % secsToDays) / secsToHours);
+      const mins = Math.floor((diffInSecs % secsToHours) / secsToMins);
+      const secs = diffInSecs % secsToMins;
+
+      return {
+        days,
+        hours,
+        mins,
+        secs,
+      };
+    }
+
+    const interval = setInterval(() => {
+      const { days, hours, mins, secs } = getDateTillEvent();
+      if (days <= 0 && hours <= 0 && mins <= 0 && secs <= 0) {
+        clearInterval(interval);
+        setCount({ days: 0, hours: 0, mins: 0, secs: 0 });
+      } else {
+        setCount({ days, hours, mins, secs });
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [date]);
 
   return (
     <div className='bg-white shadow-lg rounded-lg p-6 max-w-2xl mx-auto'>
       <h2 className='text-3xl font-bold text-center text-gray-800 mb-6'>
-        Countdown to Our Big Day
+        Solo faltan
       </h2>
       <div className='flex text-2xl justify-center'>
-        {toMexicoLocale}
-        <span className='ml-4 text-indigo-600'>Till Big Day!</span>
+        {`${count.days}d:${count.hours}h:${count.mins}m:${count.secs}s`}
+        <span className='ml-4 text-indigo-600'>para el gran día!</span>
       </div>
     </div>
   );
