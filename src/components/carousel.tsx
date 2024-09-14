701f3bfcd3223b2d03292fa0bae1a5c3811382b1
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-09-14 13:21:24 -0600

    extra image

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -4,13 +4,17 @@ import React from 'react';
 import Slider, { type Settings } from 'react-slick';
 import { IKImage } from 'imagekitio-react';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const PATHS = ['/carousel-4.jpg', '/carousel-1.jpg', '/carousel-2.jpg', '/carousel-3.jpg'];
+const PATHS = [
+  '/carousel-4.jpg',
+  '/carousel-1.jpg',
+  '/carousel-2.jpg',
+  '/carousel-3.jpg',
+];
 
 const Carousel = () => {
-
   const settings: Settings = {
     dots: true,
     infinite: true,
@@ -25,6 +29,16 @@ const Carousel = () => {
 
   return (
     <div className='w-full'>
+      <IKImage
+        urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL}
+        path='full-sized.jpg'
+        className='md:max-h-[100vh] p-10 object-cover object-[center_bottom]' 
+        transformation={[{ width: '1200', height: '1600' }]}
+        width={1200}
+        height={1600}
+        loading='lazy'
+        alt='Clau&fer-full-sized'
+      />
       <Slider {...settings}>
         {PATHS.map((_, index) => (
           <div key={index} className='md:px-10'>
@@ -32,7 +46,7 @@ const Carousel = () => {
               urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL}
               path={PATHS[index]}
               className='md:max-h-[100vh] object-cover object-[center_top]'
-              transformation={[{ width: '1600', height: '1200'}]}
+              transformation={[{ width: '1600', height: '1200' }]}
               height={1200}
               width={1600}
               loading={index > 0 ? 'lazy' : undefined}
